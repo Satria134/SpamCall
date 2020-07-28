3690b33b4db3e52e514fa7a710417c125efb8d70
Satria134 <68810122+Satria134@users.noreply.github.com>
2020-07-28 10:11:19 +0700

    Update call.py

--- call.py
+++ call.py
@@ -41,7 +41,7 @@ def bersih():
 def balik():
     d = input("\033[1;97mCoba lagi? (y/t): ")
     if d == "y":
-       subprocess.call("python spam.py",shell=True)
+       subprocess.call("python call.py",shell=True)
     elif d == "t":
          print ("\033[1;91mExit")
          os.system("exit")
